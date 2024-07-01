f442804e7a5418a70289c7ecd69a2675548e5c52
Francmho <73557750+Francmho@users.noreply.github.com>
2024-07-01 17:14:50 +0000

    Proyecto Terminado

--- src/models.py
+++ src/models.py
@@ -7,23 +7,75 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+class Follower(Base):
+    __tablename__ = 'follower'
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+    # PK
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    # INFO
+    name = Column(String(250), nullable=False)
+    #CHILDREN |MANY TO ONE | USER
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship('user', back_populates='followers' )
+
+class User(Base):
+    __tablename__ = 'user'
+
+    # PK
+    id = Column(Integer, primary_key=True)
+    # INFO
+    user_name = Column(String(250), nullable=False)
+    first_name = Column(String(250))
+    last_name = Column(String(250))
+    email = Column(String(250), nullable=False)
+    # FK | FOLLOWER | MANY TO ONE
+    person = relationship('follower', back_populates='user' )
+    #CHILDREN | COMMENT | ONE TO MANY
+    comment = relationship('comment', back_populates='user' )
+    #CHILDREN | POST | ONE TO MANY
+    comment = relationship('post', back_populates='user' )
+
+
+class Comment(Base):
+    __tablename__ = 'comment'
+
+    # PK
+    id = Column(Integer, primary_key=True)
+    # INFO
+    comment_text = Column(String(250))
+    # FK | USER | ONE TO MANY
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship('user', back_populates='comments' )
+    # FK | POST | ONE TO MANY
+    post_id = Column(Integer, ForeignKey('post.id'))
+    post = relationship('post', back_populates='comments' )
+
+class Post(Base):
+    __tablename__ = 'post'
+
+    # PK
+    id = Column(Integer, primary_key=True)
+    # INFO
+    # FK | USER | ONE TO MANY
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship('user', back_populates='posts' )
+    #CHILDREN | COMMENT | ONE TO MANY
+    comment = relationship('comment', back_populates='post' )
+    #CHILDREN | MEDIA | ONE TO MANY
+    comment = relationship('media', back_populates='post' )
+
+class Media(Base):
+    __tablename__ = 'media'
+
+    # PK
+    id = Column(Integer, primary_key=True)
+    # INFO
+    type = Column(String(250))
+    url = Column(String(250))
+    # FK | POST | ONE TO MANY
+    post_id = Column(Integer, ForeignKey('post.id'))
+    post = relationship('post', back_populates='medias' )
+   
 
     def to_dict(self):
         return {}
